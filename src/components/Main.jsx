1dcb0e26a0a3abd733d536ce2d77356b317dd40f
macluiggy <ferrinluiggy@gmail.com>
2021-09-01 13:45:41 -0500

    trying to put in the buttons

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import {
-	container,
-	calculatorContainer,
-	display,
-	buttons, 
-} from '../scss/main.module.scss'
+import '../scss/main.scss'
+
+import btns from './buttonsArray.js';
 const Main = () => 
-			<div className={container}>
-				<div className={calculatorContainer}>
-					<div id={display}>
+			<div className='container'>
+				<div className='calculatorContainer'>
+					<div id='display'>
 						
 					</div>
-					<div className={buttons}>
-						
+					<div className='buttons'>
+						{btns.map((btn, i) => <Button key={i} {...btn} />)}
 					</div>
 				</div>
 			</div>
 
+
+const Button = ({id, value}) => 
+				<button id={id}>
+					{value}
+				</button>
 export default Main;
---
+++ src/components/buttonsArray.js
@@ -0,0 +1,59 @@
+
+let btns = 
+[
+    {
+    	id: 'clear',
+    	value: 'ac',
+    },
+    {
+    	id: 'divide',
+    },
+    {
+    	id: 'multiply',
+    },
+    {
+    	id: 'seven',
+    },
+    {
+    	id: 'eight',
+    },
+    {
+    	id: 'nine',
+    },
+    {
+    	id: 'substract',
+    },
+    {
+    	id: 'four',
+    },
+    {
+    	id: 'five',
+    },
+    {
+    	id: 'six',
+    },
+    {
+    	id: 'add',
+    },
+    {
+    	id: 'one',
+    },
+    {
+    	id: 'two',
+    },
+    {
+    	id: 'three',
+    },
+    {
+    	id: 'equals',
+    },
+    {
+    	id: 'zero',
+    },
+    {
+    	id: 'decimal',
+    },
+
+]
+
+export default btns;
